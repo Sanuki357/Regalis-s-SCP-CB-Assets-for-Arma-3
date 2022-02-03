594b9de33731ea4654ed498b635ebb29b0c825c8
rvanboxtel <romek.boxtel@gmail.com>
2022-02-03 11:19:33 +0100

    Most of the changes made by sanuki added

--- addons/room_lcz/CfgVehicles.hpp
+++ addons/room_lcz/CfgVehicles.hpp
@@ -1,7 +1,7 @@
 class CfgVehicles
 {
     class Land_ToiletBox_F; 
-    class SCPLCZ_base: Land_ToiletBox_F
+    class SCP_CB_LCZ_base: Land_ToiletBox_F
     {
         scope = 0;
         scopeCurator = 0;
@@ -10,89 +10,94 @@ class CfgVehicles
         model = "\A3\Weapons_F\empty.p3d";
         destrType="DestructNo";
         simulation = House;
-        editorCategory = "SCP_Facility";
-        editorSubcategory = "SCP_LCZ";
+        editorCategory = "SCP_CB_Facility";
+        editorSubcategory = "SCP_CB_LCZ";
         mapSize = 20.27;
     };
-//******************// \\******************************************************\\
-// Hallways & Rooms // \\ 2 = Two Way, 3 = Three Way, 4 = Four-Way, C = Corner \\
-//******************// \\******************************************************\\
-    class SCPLCZ_Hallway_2_One : SCPLCZ_base
+//******************//
+// Hallways & Rooms //
+//******************//
+    class SCP_CB_LCZ_Hallway_Two_1 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Two-Way Hallway"; // Name in editor
-        model = QPATHTOF(data\Hallway2One.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Two_1\Hallway_Two_1.p3d); // Path to model
     };
-    class SCPLCZ_Hallway_2_Two : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Two_2 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Two-Way Hallway (Low Ceiling)"; // Name in editor
-        model = QPATHTOF(data\Hallway2Two.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Two_2\Hallway_Two_2.p3d); // Path to model
     };
-    class SCPLCZ_Hallway_2_Three : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Two_3 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Two-Way Hallway (Fan)"; // Name in editor
-        model = QPATHTOF(data\Hallway2Three.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Two_3\Hallway_Two_3.p3d); // Path to model
         
     };
-    class SCPLCZ_Hallway_2_Four : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Two_4 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Two-Way Hallway (Floor Lights)"; // Name in editor
-        model = QPATHTOF(data\Hallway2Four.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Two_4\Hallway_Two_4.p3d); // Path to model
     };
-    class SCPLCZ_Hallway_3_One : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Three_1 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Three-Way Hallway"; // Name in editor
-        model = QPATHTOF(data\Hallway3One.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Three_1\Hallway_Three_1.p3d); // Path to model
+        hiddenSelections[] = {"camo_floor", "camo_ceiling", "camo_wall"};
     };
-    class SCPLCZ_Hallway_3_Two : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Three_2 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Three-Way Hallway (Maintenance)"; // Name in editor
-        model = QPATHTOF(data\Hallway3Two.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Three_2\Hallway_Three_2.p3d); // Path to model
     };
-    class SCPLCZ_Hallway_3_Three : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Three_3 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Three-Way Hallway (Floor Lights)"; // Name in editor
-        model = QPATHTOF(data\Hallway3Three.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Three_3\Hallway_Three_3.p3d); // Path to model
     };
-    class SCPLCZ_Hallway_C_One : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Corner_1 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Corner Hallway"; // Name in editor
-        model = QPATHTOF(data\HallwayCOne.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Corner_1\Hallway_Corner_1.p3d); // Path to model
+        hiddenSelections[] = {"camo_floor", "camo_ceiling", "camo_wall", "camo_door"};
     };
-    class SCPLCZ_Hallway_4_One : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Four_1 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Four-Way Hallway"; // Name in editor
-        model = QPATHTOF(data\Hallway4One.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Four_1\Hallway_Four_1.p3d); // Path to model
+        hiddenSelections[] = {"camo_floor", "camo_ceiling", "camo_wall"};
     };
-    class SCPLCZ_Hallway_4_Two : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Four_2 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Four-Way Hallway (Catwalk)"; // Name in editor
-        model = QPATHTOF(data\Hallway4Two.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Four_2\Hallway_Four_2.p3d); // Path to model
+        hiddenSelections[] = {"camo_floor", "camo_ceiling", "camo_wall", "camo_door", "camo_metal3"};
     };
-    class SCPLCZ_Hallway_4_Three : SCPLCZ_base
+    class SCP_CB_LCZ_Hallway_Four_3 : SCP_CB_LCZ_base
     {
         scope = 2;
         curatorScope = 2;
         displayName = "Four-Way Hallway (Info Desk)"; // Name in editor
-        model = QPATHTOF(data\Hallway4Three.p3d); // Path to model
+        model = QPATHTOF(data\Hallway_Four_3\Hallway_Four_3.p3d); // Path to model
+        hiddenSelections[] = {"camo_floor", "camo_ceiling1", "camo_ceiling2", "camo_wall", "camo_grey", "camo_slh_miscsigns", "camo_miscsigns", "camo_miscsigns3", "camo_miscsigns4 "};
     };
 };
