86008848360750bc0069d70dde491e825737f693
Sanuki357 <sanuki357@gmail.com>
2022-02-18 02:36:48 +1030

    Geometry LODs fix and replaced normal maps and added some specular map
    Fixed geometry LODs of Hallway_Three_1 and Hallway_Corner_1. The issue was the wrong mass designated.
    
    Replaced normal maps that looked weird, as well as added some specular maps. The textures that has been tweaked including: whitewall, white, dirtymetal, tilefloor, and concretefloor.

--- addons/room_lcz/config.cpp
+++ addons/room_lcz/config.cpp
@@ -11,14 +11,14 @@
 //***************//
 
 class Extended_InitPost_EventHandlers {
-    class SCPLCZ_Hallway_Two_3 {
-        class SCPLCZ_Hallway_Two_3_init {
+    class SCP_CB_LCZ_Hallway_Two_3 {
+        class SCP_CB_LCZ_Hallway_Two_3_init {
             init = "_unit = _this select 0; if (local _unit) then {_unit spawn {_soundCache = 'Land_HelipadEmpty_F' createVehicle position _this;_soundCache attachTo [_this,[0,0,0], 'sound_fan'];while {alive _this} do {[_soundCache, 'SCP_Fan', 10] call CBA_fnc_globalSay3d;sleep 2.95;};deleteVehicle _soundCache;};};";
         };
     };
 
-    class SCPLCZ_Hallway_Three_1 {
-        class SCPLCZ_Hallway_Three_1_init {
+    class SCP_CB_LCZ_Hallway_Three_1 {
+        class SCP_CB_LCZ_Hallway_Three_1_init {
             init = "_unit = _this select 0; if (local _unit) then {_unit spawn {_soundCache = 'Land_HelipadEmpty_F' createVehicle position _this;_soundCache attachTo [_this,[0,0,0], 'sound_drip'];while {alive _this} do {[_soundCache, 'SCP_Drip', 10] call CBA_fnc_globalSay3d;sleep 4.95;};deleteVehicle _soundCache;};};";
         };
     };
