c34bafccbb676553cb1079522db9a2f27ef7ee38
Sanuki357 <sanuki357@gmail.com>
2022-04-10 02:03:58 +0930

    functional clearanceLevelCheck.sqf
    This commit fixes clearanceLevelCheck.sqf to be functional and correctly detecting keycard within action caller's inventory.
    call clearanceLevelCheck is now added to Keypad_CardReader.

--- addons/room_things/CfgFunctions.hpp
+++ addons/room_things/CfgFunctions.hpp
@@ -1,9 +1,9 @@
 class CfgFunctions {
 	class SCP {
 		class Functions {
-			class doorOperate { file = QPATHTOF(functions\fnc_doorOperate.sqf) };
-            class buttonSound { file = QPATHTOF(functions\fnc_buttonSound.sqf) };
-			class clearanceLevelCheck { file = QPATHTOF(functions\fnc_clearanceLevelCheck.sqf) };
+			class doorOperate { file = QPATHTOF(functions\fnc_doorOperate.sqf); };
+            class buttonSound { file = QPATHTOF(functions\fnc_buttonSound.sqf); };
+			class clearanceLevelCheck { file = QPATHTOF(functions\fnc_clearanceLevelCheck.sqf); };
 		};
 	};
 };
--- addons/room_things/config.cpp
+++ addons/room_things/config.cpp
@@ -2,17 +2,14 @@
 
 #include "CfgPatches.hpp"
 #include "CfgEditorCategories.hpp"
-#include "CfgSounds.hpp"
 #include "CfgVehicles.hpp"
+#include "CfgSounds.hpp"
+
 #include "CfgWeapons.hpp"
 #include "CfgFunctions.hpp"
 
-/*
 class Extended_InitPost_EventHandlers {
-    class SCP_CB_Things_Keypad_CardReader {
-        class SCP_CB_Things_Keypad_CardReader_init {
-            init = "_this select 0 addAction ['<img image=""z\RegalisSCP_CB\addons\textures\handsymbol.paa"" size=""2.5""' /> CSTRING(CardReader_Insert), {[_this select 0, _this select 1, _this select 0 getVariable ['SCP_RequiredClearanceLevel', 0], 'button_f'] call SCP_fnc_clearanceLevelCheck]}, nil, 3, true, true, '', 'true', 1, false, '', 'button_f'];";
-        };
+    class SCP_CB_Things_base_cardReader {
+        init = "_id = _this select 0 addAction [localize "CSTRING(CardReader_Insert)", {[_this select 0, _this select 1, _this select 0 getVariable ['SCP_RequiredClearanceLevel', 0], 'button_f'] call SCP_fnc_clearanceLevelCheck }, nil, 3, true, true, '', 'true', 1, false, '', 'button_f'];";
     };
 };
-*/
