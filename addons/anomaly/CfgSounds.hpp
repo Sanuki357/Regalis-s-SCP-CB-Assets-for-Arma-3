507b19470bd39deede3c99656fb2c9061d12e5f9
Sanuki357 <sanuki357@gmail.com>
2022-08-01 21:07:23 +0930

    SCP-173 graphic and config improvement
    This commit adds proper shadow volume and fire geometry to static SCP-173 object. This also replaces NOHQ map and ADSHQ map to humbler versions, fix the missing display name, and other various config file changes.

--- addons/anomaly/CfgSounds.hpp
+++ addons/anomaly/CfgSounds.hpp
@@ -4,11 +4,4 @@
 
 class CfgSounds
 {
-    sounds[] = {};
-    class SCP_Fan
-    {
-        name = "SCP_Fan";
-        sound[] = {QPATHTOF(data\Hallway_Two_3\Fan.ogg), db+20, 1};
-        titles[] ={};
-    };
 };
--- addons/anomaly/CfgVehicles.hpp
+++ addons/anomaly/CfgVehicles.hpp
@@ -1,18 +1,19 @@
 class CfgVehicles
 {
-    class Land_ToiletBox_F; 
-    class SCP_CB_Anomaly_base: Land_ToiletBox_F
+    class ThingX;
+    class SCP_CB_Anomaly_base: ThingX
     {
+        mapSize = 0.97;
         scope = 0;
         scopeCurator = 0;
         armor = 999999;
         armorStructural = 999;
         model = "\A3\Weapons_F\empty.p3d";
+        icon = "iconObject";
         destrType = "DestructNo";
         simulation = House;
         editorCategory = "SCP_Anomalies";
         editorSubcategory = "SCP_Sentient_Static";
-        mapSize = 20.27;
     };
 
 //******************//
@@ -25,5 +26,6 @@ class CfgVehicles
         curatorScope = 2;
         displayName = CSTRING(SCP173_Static);
         model = QPATHTOF(data\SCP173\SCP173.p3d); // Path to model
+        icon = "iconObject_1x4";
     };
 };
