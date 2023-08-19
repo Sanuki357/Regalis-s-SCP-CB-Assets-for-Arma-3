1260b4967f8e13292476c21c3c4a82c329a71646
Romek <romek.boxtel@gmail.com>
2023-08-19 11:41:50 +0200

    Update config.cpp room_things

--- addons/room_things/config.cpp
+++ addons/room_things/config.cpp
@@ -1,7 +1,58 @@
 
 #include "script_component.hpp"
 
-#include "CfgPatches.hpp"
+class CfgPatches
+{
+    class ADDON
+    {
+        name = COMPONENT_NAME;
+        requiredAddons[] = {"A3_Structures_F", "RegalisSCP_CB_main"};
+        requiredVersion = REQUIRED_VERSION;
+        units[] =
+        {
+            "SCP_CB_Things_Door1",
+            "SCP_CB_Things_Door1_Button",
+            "SCP_CB_Things_Door1_Button_Inverted",
+            "SCP_CB_Things_Door1_Button_Error",
+            "SCP_CB_Things_Door1_Button_Error_Inverted",
+            "SCP_CB_Things_Door1_Button_NoRespose",
+            "SCP_CB_Things_Door1_Button_NoRespose_Inverted",
+            "SCP_CB_Things_Door1_CardReader",
+            "SCP_CB_Things_Door1_CardReader_Level1",
+            "SCP_CB_Things_Door1_CardReader_Level2",
+            "SCP_CB_Things_Door1_CardReader_Level3",
+            "SCP_CB_Things_Door1_CardReader_Level4",
+            "SCP_CB_Things_Door1_CardReader_Level5",
+            "SCP_CB_Things_Door1_Scanner",
+            "SCP_CB_Things_Door1_Numpad",
+            "SCP_CB_Things_HeavyDoor",
+            "SCP_CB_Things_HeavyDoor_Button",
+            "SCP_CB_Things_ContainmentDoor",
+            "SCP_CB_Things_ElevatorRoom_Door",
+            "SCP_CB_Things_Keypad_Button",
+            "SCP_CB_Things_Keypad_CardReader",
+            "SCP_CB_Things_Keypad_Code",
+            "SCP_CB_Things_Keypad_Scanner",
+            "SCP_CB_Things_FileCabinet",
+            "SCP_CB_Things_OfficeSeat",
+            "SCP_CB_Things_Crate1",
+            "SCP_CB_Things_Crate2",
+            "SCP_CB_Things_Crate3",
+            "SCP_CB_Things_Item_Keycard_C1",
+            "SCP_CB_Things_Item_Keycard_C2",
+            "SCP_CB_Things_Item_Keycard_C3",
+            "SCP_CB_Things_Item_Keycard_C4",
+            "SCP_CB_Things_Item_Keycard_C5"
+        };
+        weapons[] = {
+            "SCP_CB_Things_Keycard_C1",
+            "SCP_CB_Things_Keycard_C2",
+            "SCP_CB_Things_Keycard_C3",
+            "SCP_CB_Things_Keycard_C4",
+            "SCP_CB_Things_Keycard_C5"
+        };
+    };
+};
 #include "CfgEditorCategories.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgSounds.hpp"
