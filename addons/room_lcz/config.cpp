76d3a804cbb44747cdbd2fcb1fa917874eb0fe2a
Romek <romek.boxtel@gmail.com>
2023-08-19 11:41:22 +0200

    Update config.cpp room_lcz

--- addons/room_lcz/config.cpp
+++ addons/room_lcz/config.cpp
@@ -1,7 +1,49 @@
 
 #include "script_component.hpp"
 
-#include "CfgPatches.hpp"
+class CfgPatches
+{
+    class ADDON
+    {
+        name = COMPONENT_NAME;
+        requiredAddons[] = {"A3_Structures_F", "RegalisSCP_CB_main"};
+        requiredVersion = REQUIRED_VERSION;
+        units[] =
+        {
+            "SCP_CB_LCZ_Hallway_Two_1",
+            "SCP_CB_LCZ_Hallway_Two_2",
+            "SCP_CB_LCZ_Hallway_Two_3",
+            "SCP_CB_LCZ_Hallway_Two_4",
+            "SCP_CB_LCZ_Hallway_Two_5",
+            "SCP_CB_LCZ_Hallway_Three_1",
+            "SCP_CB_LCZ_Hallway_Three_2",
+            "SCP_CB_LCZ_Hallway_Three_3",
+            "SCP_CB_LCZ_Hallway_Corner_1",
+            "SCP_CB_LCZ_Hallway_Corner_2",
+            "SCP_CB_LCZ_Hallway_Corner_Elevator",
+            "SCP_CB_LCZ_Hallway_Four_1",
+            "SCP_CB_LCZ_Hallway_Four_2",
+            "SCP_CB_LCZ_Hallway_Four_3",
+            "SCP_CB_LCZ_Hallway_Tesla",
+			"SCP_CB_LCZ_Room_Archive",
+			"SCP_CB_LCZ_Room_Endroom",
+			"SCP_CB_LCZ_Room_Storeroom",
+			"SCP_CB_LCZ_Room_327",
+			"SCP_CB_LCZ_Room_1162",
+            "SCP_CB_LCZ_Piece_WhiteWall",
+            "SCP_CB_LCZ_Piece_WhiteWall_Wide",
+            "SCP_CB_LCZ_Piece_WhiteWall_Concave",
+            "SCP_CB_LCZ_Piece_WhiteWall_Floorlight",
+            "SCP_CB_LCZ_Piece_WhiteWall_Doorway",
+            "SCP_CB_LCZ_Piece_WhiteWall_Gateway",
+            "SCP_CB_LCZ_Piece_Floor_Concrete",
+            "SCP_CB_LCZ_Piece_Floor_Concrete_Big",
+            "SCP_CB_LCZ_Piece_Floor_Tile",
+            "SCP_CB_LCZ_Piece_Floor_Tile_Big"
+        };
+        weapons[] = {};
+    };
+};
 #include "CfgEditorCategories.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgSounds.hpp"
